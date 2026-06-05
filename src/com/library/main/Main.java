c5928f70028abdc41f551ecfdbe971d3f1d884f0
manhtoan123 <23010539@st.phenikaa-uni.edu.vn>
2026-06-05 10:44:03 +0700

    Add temporary main() entry point for quick code checks

--- src/com/library/main/Main.java
+++ src/com/library/main/Main.java
@@ -1,5 +1,9 @@
 package com.library.main;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("[TEMP MAIN] App started");
+        // TODO: Gọi thử các class/hàm bạn muốn kiểm tra ở đây.
+        // Ví dụ: new SomeService().someMethod();
+    }
 }
-123
